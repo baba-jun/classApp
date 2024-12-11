1ca2e9d87de4a8edfbc5435bb464827dae6c96c9
baba-jun <msk3059i@gmail.com>
2024-12-11 15:52:54 +0900

    first commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/')
+      .then(response => response.json())
+      .then(data => setMessage(data.message || 'No message available'))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
