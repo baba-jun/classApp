8112b752d386f0150563826923fff19ef53116d2
baba-jun <msk3059i@gmail.com>
2024-12-12 21:15:26 +0900

    環境変数の設定

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [message, setMessage] = useState('');
+  const backend_url = process.env.BACKEND_URL || 'http://localhost:8080'
 
   useEffect(() => {
-    fetch('https://backend-rose-ten.vercel.app')
+    fetch(backend_url)
       .then(response => response.json())
       .then(data => setMessage(data.message || 'No message available'))
       .catch(error => console.error('Error:', error));
-  }, []);
+  }, [backend_url]);
 
   return (
     <div>
