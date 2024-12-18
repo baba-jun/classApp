08276ca56a4adf0358a3692f0850a5f5fdef6b80
baba-jun <msk3059i@gmail.com>
2024-12-18 22:52:43 +0900

    prelineの導入

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+import prelinePlugin from 'preline/plugin';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './node_modules/preline/preline.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    prelinePlugin,
+  ],
+}
+
