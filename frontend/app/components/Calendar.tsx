18e85968e84166add290c5e63c431171e9497146
baba-jun <msk3059i@gmail.com>
2024-12-19 22:15:10 +0900

    表示するCalendarの変更

--- frontend/app/components/Calendar.tsx
+++ frontend/app/components/Calendar.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Calendar = () => {
   return (
-    <iframe
-      src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Asia%2FTokyo&src=dXJiYW5wYXJrMDFAZ21haWwuY29t&src=amEuamFwYW5lc2UjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%230B8043"
-      style={{ border: 'solid 1px #777' }}
-      width="800"
-      height="600"
-      frameBorder="0"
-      scrolling="no"
-    ></iframe>
+    <iframe 
+      src="https://calendar.google.com/calendar/embed?src=2ac4536b9b7fe0b05e4b1a56e090f4d24668f470c2d7f5595c637748a86b952a%40group.calendar.google.com&ctz=Asia%2FTokyo" 
+      style={{ border: 0 }} 
+      width="800" 
+      height="600" 
+      frameBorder="0" 
+      scrolling="no">
+    </iframe>
   );
 }
 
